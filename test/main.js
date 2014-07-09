3285d8ff34c9bfbb5ee80f81bbb4de48f6d6069f
Steve Lacy <me@slacy.me>
2014-07-09 11:30:05 -0700

    initial commit

---
+++ test/main.js
@@ -0,0 +1,23 @@
+var fs = require('fs');
+var path = require('path');
+var should = require('should');
+
+var fixmyjson = require('../lib/fixmyjson');
+var normalFixture = fs.readFileSync(path.resolve(__dirname, './fixtures/normal.json'));
+var normalExpected = fs.readFileSync(path.resolve(__dirname, './expected/normal.json'), 'utf8');
+var optionsExpected = fs.readFileSync(path.resolve(__dirname, './expected/options.json'), 'utf8');
+
+describe('fixmyjson', function(){
+  it('should clean and order json', function(done){
+    fixmyjson(normalFixture, {}, function(data){
+      data.should.equal(normalExpected);
+      done();
+    });
+  });
+  it('should use options', function(done){
+    fixmyjson(normalFixture, {indent_size: 4}, function(data){
+      data.should.equal(optionsExpected);
+      done();
+    });
+  });
+});
